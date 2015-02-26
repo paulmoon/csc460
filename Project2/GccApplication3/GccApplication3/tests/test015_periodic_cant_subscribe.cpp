045cb2643803634dd13365cfcf2a88a387af2013
Paul Moon <moon.haein@gmail.com>
2015-02-25 23:01:17 -0800

    Added 2 tests for 1) testing periodic tasks cannot subscribe to a service and 2) RR tasks subscribing to a periodic publisher. Also renamed tests for clarity.

---
+++ Project2/GccApplication3/GccApplication3/tests/test015_periodic_cant_subscribe.cpp
@@ -0,0 +1,59 @@
+#ifdef USE_TEST_015
+
+/*
+    Desired Trace: None
+*/
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#include "os.h"
+#include "uart/uart.h"
+#include "trace/trace.h"
+#include "../profiler.h"
+#include "../error_code.h"
+
+SERVICE* services[1];
+
+void p() {
+    for(;;) {
+        int16_t v;
+        Service_Subscribe(services[0], &v);
+        add_to_trace(v);
+
+        // This should never happen since periodic tasks cannot subscribe.
+        if (v > 1) {
+            print_trace();
+            Task_Terminate();
+        }
+
+        Task_Next();
+    }
+}
+
+void r() {
+    int16_t count = 0;
+
+    for (;;) {
+        Service_Publish(services[0], count);
+        count++;
+        Task_Next();
+    }
+}
+
+extern int r_main(){    
+    uart_init();
+    set_trace_test(15);
+
+    services[0] = Service_Init();
+
+    Task_Create_RR(r, 0);
+    // Periodic tasks cannot subscribe. Expected: OS abort with code ERR_RUN_12_PERIODIC_CANT_SUBSCRIBE
+    Task_Create_Periodic(p, 10, 10, 2, 5);
+
+    Task_Terminate();
+    return 0;
+}
+
+#endif
---
+++ Project2/GccApplication3/GccApplication3/tests/test016_service_RR_subscribe.cpp
@@ -0,0 +1,66 @@
+#ifdef USE_TEST_016
+
+/*
+    Desired Trace:
+    T016;1;1;2;2;3;3;4;4;5;5;...;19;19;20;20;
+*/
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#include "os.h"
+#include "uart/uart.h"
+#include "trace/trace.h"
+#include "../profiler.h"
+#include "../error_code.h"
+
+SERVICE* services[3];
+int16_t count = 1;
+
+void p() {
+    for (;;) {
+        Service_Publish(services[i], &v);
+        count++;
+        Task_Next();
+    }
+}
+
+void r() {
+	int16_t v;
+	int16_t service_i = Task_GetArg();
+
+    for (;;) {
+		Service_Subscribe(services[service_i], &v);
+		add_to_trace(v);
+
+        if (count > 20) {
+            Service_Publish(services[3], 40);
+            Task_Terminate();            
+        }
+
+        Task_Next();
+    }
+}
+
+extern int r_main(){    
+    uart_init();
+    set_trace_test(16);
+
+    services[0] = Service_Init();
+    services[1] = Service_Init();
+    services[2] = Service_Init();
+    
+    Task_Create_RR(r, 0);
+    Task_Create_RR(r, 0);
+    Task_Create_Periodic(p, 0, 20, 2, 5);
+   
+    int16_t v;
+    Service_Subscribe(services[2], &v);
+    add_to_trace(v);
+    print_trace();
+    
+    Task_Terminate();
+    return 0;
+}
+
+#endif
